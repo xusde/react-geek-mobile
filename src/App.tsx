6d924b2dc479a5cf36329b025e4ee0e9e0963681
Pete Xu <pengx@alum.lehigh.edu>
2023-12-15 09:52:20 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd-mobile";
+const App = () => {
+  return (
+    <div>
+      App
+      <Button color="success">按钮</Button>
+    </div>
+  );
+};
+
+export default App;
